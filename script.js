bb1b60a56137161896927bd4044a39f18a01572d
Sam Uwog <suwoghiren@cristoreyny.org>
2017-02-03 14:52:10 +0000

    I added beautiful colors

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Write your function calls below this line
+setColor(2,0,"purple");
+setColor(0,9,"red");
+setColor(1,1,"black");
+setColor(0,0,"grey");
+setColor(3,1,"navy");
+setColor(4,0,"blue");
+setColor(5,1,"beige");
+setColor(6,0,"tan");
